1ed3fa0ede663cffa38d428b5d95771c08d3078b
crendai <159742776+crendai@users.noreply.github.com>
2024-05-23 22:13:32 -0500

    Update CurrencyMenu.java

--- src/CurrencyMenu.java
+++ src/CurrencyMenu.java
@@ -1,1 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class CurrencyMenu {
+    private static Map<String, String> countryCurrencyMap = new HashMap<>();
+
+    static {
+        countryCurrencyMap.put("Estados Unidos", "USD"); 
+        countryCurrencyMap.put("México", "MXN"); 
+        countryCurrencyMap.put("Eurozona", "EUR"); 
+        countryCurrencyMap.put("Colombia", "COP"); 
+        countryCurrencyMap.put("Brasil", "BRL"); 
+        countryCurrencyMap.put("Perú", "PEN"); 
+        countryCurrencyMap.put("Reino Unido", "GBP"); 
+        countryCurrencyMap.put("Venezuela", "VES"); 
+        countryCurrencyMap.put("Argentina", "ARS"); 
+    }
+
+    public static String selectCurrency(Scanner scanner) {
+        int i = 1;
+        for (Map.Entry<String, String> entry : countryCurrencyMap.entrySet()) {
+            System.out.println(i + ". " + entry.getKey() + " (" + entry.getValue() + ")");
+            i++;
+        }
+        int choice = scanner.nextInt();
+        int index = 1;
+        for (Map.Entry<String, String> entry : countryCurrencyMap.entrySet()) {
+            if (index == choice) {
+                return entry.getValue();
+            }
+            index++;
+        }
+        return null;
+    }
+}
