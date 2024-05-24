424b0eca57261d87f953229c67bdee2ab3e2d9ca
crendai <159742776+crendai@users.noreply.github.com>
2024-05-23 22:10:47 -0500

    Update CurrencyConverter.java

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,1 +1,6 @@
-
+public class CurrencyConverter {
+    public static double convert(String fromCurrency, String toCurrency, double amount) {
+        double rate = ExchangeRateAPI.getExchangeRate(fromCurrency, toCurrency);
+        return amount * rate;
+    }
+}
